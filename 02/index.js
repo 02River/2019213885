25f3463ec497609a40d17895203b4de2be5b7394
02River <2425608844@qq.com>
2022-01-05 19:20:30 +0800

    Add files via upload

---
+++ 02/index.js
@@ -0,0 +1,186 @@
+function f1mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：微机原理与接口技术";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：金汉均";
+    var when=document.getElementById("time");
+    when.innerText="时间：周一3-4节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N113";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业必修课";
+}
+function f2mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：专业英语";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：朱瑄";
+    var when=document.getElementById("time");
+    when.innerText="时间：周一7-8节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N211";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业选修课";
+}
+function f3mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：传播中的法与理";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：陈科 等";
+    var when=document.getElementById("time");
+    when.innerText="时间：周一11-12节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：网课";
+    var such=document.getElementById("such");
+    such.innerText="类型：通识核心课";
+}
+function f4mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：web程序设计/编译原理";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：涂新辉/蔡霞";
+    var when=document.getElementById("time");
+    when.innerText="时间：周二1-2节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N524";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业选修课";
+}
+function f5mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：进化社会科学概论";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：罗力群";
+    var when=document.getElementById("time");
+    when.innerText="时间：周二7-8节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：8108";
+    var such=document.getElementById("such");
+    such.innerText="类型：通识核心课";
+}
+function f6mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：人工智能";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：戴上平";
+    var when=document.getElementById("time");
+    when.innerText="时间：周三1-2节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N113";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业选修课";
+}
+function f7mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：操作系统";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：喻莹";
+    var when=document.getElementById("time");
+    when.innerText="时间：周三3-4节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N213";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业必修课";
+}
+function f8mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：微机原理与接口技术";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：金汉均";
+    var when=document.getElementById("time");
+    when.innerText="时间：周三7-8节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N113";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业必修课";
+}
+function f9mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：习近平新时代中国特色社会主义思想概论";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：吕惠东";
+    var when=document.getElementById("time");
+    when.innerText="时间：周四3-4节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N223";
+    var such=document.getElementById("such");
+    such.innerText="类型：通识选修课";
+}
+function f10mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：web程序设计";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：涂新辉";
+    var when=document.getElementById("time");
+    when.innerText="时间：周四5-6节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N108";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业选修课";
+}
+function f11mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：编译原理";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：蔡霞";
+    var when=document.getElementById("time");
+    when.innerText="时间：周四7-8节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N211";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业选修课";
+}
+function f12mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：跟着电影学传媒";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：陈科";
+    var when=document.getElementById("time");
+    when.innerText="时间：周四9-10节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N7209";
+    var such=document.getElementById("such");
+    such.innerText="类型：通识选修课";
+}
+function f13mouseover()
+{document.getElementById("no2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：操作系统";
+    var who=document.getElementById("teacher");
+    who.innerText="教师：喻莹";
+    var when=document.getElementById("time");
+    when.innerText="时间：周五1-2节";
+    var where=document.getElementById("locate");
+    where.innerText="地点：N213";
+    var such=document.getElementById("such");
+    such.innerText="类型：专业必修课";
+}
+
+
+
+
+
+function mouseout()
+{   document.getElementById("no2").style.visibility="hidden";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：";
+
+}
